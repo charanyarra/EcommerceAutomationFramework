cb7b5be28f3f8b13ce312dc889cfe2a2132ed0b5
charanyarra <charanyarra@gmail.com>
2026-04-29 21:35:12 +0530

    Added explicit wait utility for stable automation

---
+++ src/main/java/com/ecommerce/framework/utils/WaitUtils.java
@@ -0,0 +1,23 @@
+package com.ecommerce.framework.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitUtils {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public WaitUtils(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void waitForElementVisible(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+}
